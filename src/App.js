8f036b49a370911e0b77dbc0cb871a303461fb28
quocviet <quocviet.ce@gmail.com>
2020-05-12 23:26:13 +0700

    Bai 9

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = ['Mua bim bim', 'Đi đá bóng', 'Đi đổ xăng'];
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItems.map((item, i) => <TodoItem title={item} key={i}/>)
+        }
+      </div>
+    );
+  }
+  
 }
 
 export default App;
