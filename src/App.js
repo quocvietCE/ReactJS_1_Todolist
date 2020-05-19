592ad12bf994962b7763f92caa8be243715b2ab8
viet.nguyen.successsoftware <viet.nguyen@successsoftware.global>
2020-05-19 17:18:16 +0700

    Bài 15 - Finish TODO App (Part 1)

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     }
   }
 
-  onItemClick = (itemInput) => {
+  onItemClick1 = (itemInput) => {
     console.log('item: ', itemInput);
     const newTodoItem = this.state.todoItems.map((item) => {
       if(item.title === itemInput.title){
@@ -26,11 +26,33 @@ class App extends Component {
   this.setState({todoItems: newTodoItem})
   }
 
+  onItemClick2(itemInput) {
+    return (event) => {
+      const isComplete = itemInput.isComplete;
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(itemInput);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...itemInput,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index+1)
+        ]
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         {this.state.todoItems.length > 0 ? this.state.todoItems.map((item, i) => (
-          <TodoItem item={item} key={i} onClick={()=>this.onItemClick(item)}/>
+          <TodoItem 
+            item={item} key={i} 
+            // onClick={()=>this.onItemClick1(item)}
+            onClick={this.onItemClick2(item)}
+          />
         )) : 'Nothing Here'}
       </div>
     );
