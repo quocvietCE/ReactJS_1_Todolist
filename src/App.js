8e3881a799e50a47999b8d8796111943ee3c2c29
viet.nguyen.successsoftware <viet.nguyen@successsoftware.global>
2020-05-19 14:47:51 +0700

    Bài 13 - Handling events

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import TodoItem from './components/TodoItem';
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
-      { title: "Mua bim bim", isComplete: true },
-      { title: "Đi đá bóng", isComplete: false },
-      { title: "Đi đổ xăng", isComplete: false },
-    ];
+    this.state = {
+      todoItems : [
+        { title: "Mua bim bim", isComplete: true },
+        { title: "Đi đá bóng", isComplete: false },
+        { title: "Đi đổ xăng", isComplete: false },
+      ]
+    }
   }
+
+  onItemClick = (itemInput) => {
+    console.log('item: ', itemInput);
+    const newTodoItem = this.state.todoItems.map((item) => {
+      if(item.title === itemInput.title){
+        item.isComplete = !item.isComplete;
+      }
+      return item;
+  });
+  console.log('newTodoItem: ', newTodoItem);
+  this.setState({todoItems: newTodoItem})
+  }
+
   render() {
     return (
       <div className="App">
-        {this.todoItems.length > 0 ? this.todoItems.map((item, i) => (
-          <TodoItem item={item} key={i} />
+        {this.state.todoItems.length > 0 ? this.state.todoItems.map((item, i) => (
+          <TodoItem item={item} key={i} onClick={()=>this.onItemClick(item)}/>
         )) : 'Nothing Here'}
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import './TodoItem.css';
 
 class TodoItem extends Component {
+  onItemClick = () => {
+    console.log(this.props.item)
+  }
     render() {
-        const {item} = this.props;
+        const {item, onClick} = this.props;
         var className = "TodoItem";
         if (item.isComplete) {
             className += " TodoItem-Complete";
         }
           return (
-            <div className={className}>
+            <div className={className} onClick={onClick}>
               <p>{item.title}</p>
             </div>
           );
